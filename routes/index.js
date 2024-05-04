b604341d2590bff409f1793937b1ffef0eb87195
OMAR CHURQUINA <oochurquina@hotmail.com>
2024-05-04 10:36:07 -0300

    cambios locales

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,6 @@ import express from 'express'
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.get('/',(request,response,next)=>{
-    response.send('hola mundo');
-    next();
-})
-
-router.get('/nosotros',(request,response,next)=>{
-    response.render('nosotros');
-    next();
-})
-
-router.get('/contacto',(request,response,next)=>{
-    response.send('Contacto') 
-})
-
-=======
 router.get('/', (request, response, next) => {
     response.render('inicio');
     next();
@@ -31,5 +15,4 @@ router.get('/nosotros', (request, response, next) => {
     next();
 })
 
->>>>>>> 2bf05bee1abe204037dcdfec4b1fd6231422be1f
 export default router;
